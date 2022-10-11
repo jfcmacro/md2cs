82eb8e796240c3d717f73a05dfb06ae8bfc5e6ce
Juan Cardona <juan_cardona@epam.com>
2022-10-11 10:55:20 -0500

    Starting point

---
+++ README.md
@@ -0,0 +1,6 @@
+# md2cs (Markdown to 2 Coding Stories)
+
+This project takes a markdown with a script that contains all stories
+for a coding stories structures.
+
+
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include "../include/md2cs_config.h"
+
+int
+main(int argc, char *argv[]) {
+  std::cout << "md2cs Version: "
+            << md2cs_VERSION_MAJOR
+            << "."
+            << md2cs_VERSION_MINOR
+            << " is not ready yet" << std::endl;
+  return EXIT_SUCCESS;
+}
